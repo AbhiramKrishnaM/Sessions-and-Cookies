acb5646861a55d0a9daeb0ef82b4ac1ec3362f03
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2022-08-16 22:04:44 +0530

    feat: added mongoose db

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
+require("dotenv").config();
 const express = require("express");
 
 const session = require("express-session");
 
+const MongoDBSession = require("connect-mongodb-session")(session);
+
 const app = express();
+
+// connecting mongo db
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+
+const db = mongoose.connection;
+
+db.on("error", (error) => console.error(error));
+
+db.once("open", () => console.log("Connected to database"));
+
+//
+
+const store = new MongoDBSession({
+  uri: process.env.DATABASE_URL,
+  collection: "mySession",
+});
 
 app.use(
   session({
     secret: " Key that will sign the cookie ",
     resave: false,
     saveUninitialized: false,
+    store: store,
   })
 );
 
 app.get("/user", (req, res) => {
   req.session.isAuth = true;
-  console.log(req.session);
-  console.log(req.session.id);
   res.send("Hello");
 });
 
