5a80d406ef7b48b2f11861a28a37fc34dee59d42
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2022-08-18 10:34:11 +0530

    feat: refactored login route

--- routes/auth.js
+++ routes/auth.js
@@ -8,23 +8,23 @@ router.post("/login", async (req, res) => {
   const form = req.body;
 
   try {
-    // call the url
     const response = await axios.post(
       process.env.BASE_MMIP_URL + "/api/admin-login",
       form
     );
 
     if (response.status === 200) {
-      if (!req.session.user) {
-        req.session.user = response.data.data;
-
-        res.status(200).json({ msg: "Authenticated" });
+      if (req.session.user) {
+        res.send(response.data);
       } else {
-        res.status(401).json("Already a user");
+        req.session.user = response.data.data;
+        res.send(response.data);
       }
+    } else {
+      res.send(response.data);
     }
   } catch (err) {
-    res.status(401).send("Bad credentials");
+    res.send(err.response.data);
   }
 });
 
